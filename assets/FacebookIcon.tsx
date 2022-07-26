028177aba904e93a1dded3ce5a4203d489570135
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2022-07-26 22:51:38 +0700

    create component icon

---
+++ assets/FacebookIcon.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+function FacebookIcon(): ReactElement {
+    return (
+        <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.83372 3.32083H9.58333V0.140833C9.28148 0.0975 8.24338 0 7.03438 0C4.51178 0 2.78373 1.65583 2.78373 4.69917V7.5H0V11.055H2.78373V20H6.19671V11.0558H8.86784L9.29186 7.50083H6.19591V5.05167C6.19671 4.02417 6.46182 3.32083 7.83372 3.32083Z" fill="white" />
+        </svg>
+    );
+}
+
+export default FacebookIcon;
